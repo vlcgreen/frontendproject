fa457ce0b38905bacd27768478e0e9ed86f9a2f5
Victoria Green <victoria.cousan@gmail.com>
2021-05-18 15:51:53 -0500

    finished js for amazonfetch

--- js/celebrator.js
+++ js/celebrator.js
@@ -1,21 +1,52 @@
 //celebrator web app 
 
+//keywords:
+let theme = 'MardiGras'
+
 
 async function amazonPull(){
 
     let resultsarr = []
 
-    const response = await fetch(`https://api.rainforestapi.com/request?api_key=${apiKey}type=search&amazon_domain=amazon.com&search_term=luau+decorations&sort_by=most_recent`)
+    const response = await fetch(`https://api.rainforestapi.com/request?api_key=${apiKey}&type=search&amazon_domain=amazon.com&search_term=${theme}+decorations&sort_by=most_recent`)
     const searchData = await response.json();
     searchResults = searchData;
-    // console.log(searchResults)
+    console.log(searchResults)
     searchData.search_results.forEach(item =>{
-        const {price, title, image} = item
-        resultsarr.push({price, title, image})
+        const {price, title, image, link} = item
+        resultsarr.push({price, title, image, link})
     })
-    console.log(resultsarr)
+    // console.log(resultsarr)
+
+    for (let i=0; i<=resultsarr.length; i++){
+        let title = resultsarr[i].title
+        let image = resultsarr[i].image
+        let link = resultsarr[i].link
+        let price = resultsarr[i].price.raw
+        createCard(title, price, image, link)
+    }
+
 }
 
 amazonPull();
 
-//api key isn't in here because I didn't want to waste fetches with broken code. But we can test maybe 10x a day just to see if everything we want is happening. 
+//api key isn't in here because I didn't want to waste fetches with broken code.
+
+
+
+
+function createCard(title, price, image, link) {
+    var body = document.body;
+    var div = document.createElement("div");
+    body.append(div);
+    div.innerHTML = `<div class="card" style="width: 18rem;"><a href ="${link}">
+      <img class="card-img-top" src="${image}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${price}</h5>
+        <p class="card-text">${title}</p>
+        </a>
+      </div>
+    </div>`;
+  }
+  
+
